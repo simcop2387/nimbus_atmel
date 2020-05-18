c7f2ecb589a1caa18d0298313df8c858ef950625
Ryan Voots <simcop2387@simcop2387.info>
2020-05-17 20:25:47 -0700

    Starting protocol decoding

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,15 @@
 #include <Arduino.h>
 #include <Wire.h>
 
+#define SER_COM_GAUGE 0x01
+#define SER_COM_DISP  0x02
+#define SER_COM_BRIGHT 0x03
+#define SER_COM_AUDIO  0x04
+// This should in theory get sent to the esp when a button is hit
+#define SER_COM_BUTTON 0x70
+// A command to flush the buffer and pretend like we never got any data
+#define SER_COM_RESET 0xFF
+
 const int output_pins[]={LED_BUILTIN, 16};
 
 const uint8_t display_ctl_addr[]  = {0x3A, 0x3E, 0x3C, 0x38};
@@ -9,6 +18,7 @@ const uint8_t display_data_addr[] = {0x3B, 0x3F, 0x3D, 0x39};
 const uint8_t gauge_addr[] = {0xC8, 0xE8, 0xA8, 0x88};
 
 uint8_t display_buffers[4][41];
+uint8_t brightness = 255;
 
 struct gauge_t {
   uint8_t cur_pos;
@@ -83,12 +93,69 @@ void set_write_gauge(uint8_t dial, uint8_t direction, uint16_t value) {
 
 uint8_t serial_pos = 0;
 uint8_t serial_end = 0;
-uint8_t serial_buf[128] = {0};
+uint8_t serial_buf[64] = {0};
+
+#define SER_PEEK() serial_buf[serial_pos]
+// This fails to consume a byte if we're at the end of the buffer, but will return the last byte
+inline uint8_t SER_CONSUME() {
+  uint8_t temp = serial_buf[serial_pos];
+  
+  if (serial_pos != serial_end) {
+    serial_pos = (serial_end + 1) % sizeof(serial_buf);
+  }
+
+  return temp;
+}
+// How many bytes are available
+#define SER_AVAIL() (serial_end - serial_pos >= 0 ? serial_end - serial_pos : sizeof(serial_buf) + (serial_end - serial_pos))
 
 void check_decode_serial() {
-  if (Serial.available() > 0) {
-    
+  size_t ready = Serial.available();
+  if (ready > 0) {
+    size_t to_read = ready;
+    size_t total_read = 0;
+    size_t bytes_read;
+
+    while(total_read < to_read) {
+      size_t read_len = to_read - total_read <= sizeof(serial_buf) - serial_end // Don't go past the end of the ring buffer
+                        ? to_read - total_read // read up however many bytes we have left
+                        : sizeof(serial_buf) - serial_end; // read up until we hit the end of the buffer instead
+      bytes_read = Serial.readBytes((char *) &serial_buf[serial_end], to_read);
+      total_read += bytes_read; // update our position on the port
+
+      serial_end = (to_read + serial_end) % sizeof(serial_buf);
+    }
   }
+
+  size_t avail = SER_AVAIL();
+  if (avail > 0) {
+    switch(SER_PEEK()) {
+      case SER_COM_GAUGE:
+        if (avail >= 3) {
+          SER_CONSUME(); // drop the command
+          uint8_t gauge = SER_CONSUME();
+          uint8_t new_pos = SER_CONSUME();
+
+          // ignore any other gauges
+          if (gauge < 4) {
+            gauge_status[gauge].tgt_pos = new_pos;
+          }
+        }
+        break;
+      case SER_COM_BRIGHT:
+        if (avail >= 2) {
+          SER_CONSUME(); // drop the command
+          brightness = SER_CONSUME();
+        }
+        break;
+      case SER_COM_DISP:
+      case SER_COM_AUDIO:
+      default: // Shouldn't happen, we'll just consume a byte
+        SER_CONSUME();
+        break;
+    }
+  }
+
 }
 
 
