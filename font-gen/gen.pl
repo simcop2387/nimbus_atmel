40d2ec79300bddae18f530a2b51a06993053b33b
Ryan Voots <simcop2387@simcop2387.info>
2020-05-20 19:01:22 -0700

    Font rendering is mostly ready

--- font-gen/gen.pl
+++ font-gen/gen.pl
@@ -22,18 +22,50 @@ sub transpose_glyph {
   my $g = shift;
 
   my @raw_bits = map {[split('', substr(sprintf("%032b", $_), 0, $g->width))]} $g->bitmap->@*;
-  print ddc(\@raw_bits);
+  my @new_bits;
 
+  for my $row (0..$#raw_bits) {
+    for my $col (0..$g->width-1) {
+      $new_bits[$col][$row] = $raw_bits[$row][$col];
+    }
+  }
+
+  # they're not cis bits
+  my @trans_bits = grep {$_ ne "0b00000000"} map {"0b".join '', $_->@*} @new_bits;
+
+  push @trans_bits, "0b00000000"; # Always ensure we have at least one column, and that it's blank at the end.  This adds kerning and prevents issues with some characters that are blank
+  push @trans_bits, "0b00000000" if $g->name eq ' '; # make sure space is at least two pixels wide.
+  unshift @trans_bits, "0b00000000" if $g->name eq '.'; # center the . a bit to balance things
+
+#  print Dumper(\@trans_bits);
+  return {width => scalar @trans_bits, columns => \@trans_bits}; 
 }
 
-my $glyph={};
+my @bytes;
+my @widths;
 
 my $font = Font::PCF->open( "/usr/share/fonts/X11/misc/clR5x8.pcf.gz" );
 
-for my $char (ord" "..ord"~") {
+for my $char (32..127) {
   my $g = $font->get_glyph_for_char( chr $char );
-  $glyph->{$char} = transpose_glyph $g; # TODO transpose, etc.
-#  printf "%s: %d [%d, %d] x [%d, %d]\n", chr($char), $g->width, $g->left_side_bearing, $g->right_side_bearing, $g->ascent, $g->descent;
+  my $glyph = transpose_glyph $g;
 
-#  printbits $_ for $glyph->bitmap->@*;
+  push @bytes, $glyph->{columns}->@*;
+  push @widths, $glyph->{width};
 }
+
+my $template = <<"EOT";
+
+uint8_t font_glyphs[] = {
+    %B%
+};
+uint8_t font_starts[] = {%S%};
+uint8_t font_widths[] = {%W%};
+
+EOT
+
+my $B = join "", (join ",\n    ", @bytes);
+my $total = 0;
+my $S = join ', ', map {$total += $_; sprintf "0x%03X", $total} @widths;
+my $W = join ', ', @widths;
+print $template =~ s/%B%/$B/r =~ s/%W%/$W/r =~ s/%S%/$S/r;
