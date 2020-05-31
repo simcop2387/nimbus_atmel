5f5c8fd4abd3bf4a1b7b259838c5024b48ba874b
Ryan Voots <simcop2387@simcop2387.info>
2020-05-31 12:27:09 -0700

    Added more symbols

--- font/gen.pl
+++ font/gen.pl
@@ -10,431 +10,473 @@ use Data::Dumper::Compact 'ddc';
 use GD;
 
 my $custom_chars = {
-  0 => [ # Blank base character, save for later
-    0b00000000,
-    0b00000000,
-    0b00000000,
-    0b00000000,
-    0b00000000,
-    0b00000000,
-    0b00000000,
-    0b00000000, # screen is only 7 high, but we'll use the 8th bit to prevent code from trimming otherwise blank columns
+	0 => [ # Blank base character, save for later
+		0b00000000,
+		0b00000000,
+		0b00000000,
+		0b00000000,
+		0b00000000,
+		0b00000000,
+		0b00000000,
+		0b00000000, # screen is only 7 high, but we'll use the 8th bit to prevent code from trimming otherwise blank columns
+	],
+	1 => [ # Smiley
+		0b00000000,
+		0b01101100,
+		0b01101100,
+		0b00000000,
+		0b00000000,
+		0b10000010,
+		0b01111100,
+		0b00000000,
+	],
+	2 => [ # Frowny
+		0b00000000,
+		0b01101100,
+		0b01101100,
+		0b00000000,
+		0b00000000,
+		0b01111100,
+		0b10000010,
+		0b00000000,
+	],
+	3 => [ # Winky
+		0b00000000,
+		0b01001100,
+		0b00001100,
+		0b00000000,
+		0b00000000,
+		0b10000010,
+		0b01111100,
+		0b00000000,
+	],
+	4 => [ # Excited
+		0b00000000,
+		0b01101100,
+		0b01101100,
+		0b00000000,
+		0b11111110,
+		0b10000010,
+		0b01111100,
+		0b00000000,
+	],
+	5 => [ # ? not sure yet TODO
+		0b00000000,
+		0b01001100,
+		0b00001100,
+		0b00000000,
+		0b00000000,
+		0b10000010,
+		0b01111100,
+		0b00000000,
+	],
+	6 => [ # Mug
+		0b10001000,
+		0b10001110,
+		0b10001001,
+		0b10001001,
+		0b10001001,
+		0b10001110,
+		0b11111000,
+		0b00000000,
+	],
+	7 => [ # Music note
+		0b00011100,
+		0b00010010,
+		0b00010000,
+		0b00010000,
+		0b00010000,
+		0b01100000,
+		0b01100000,
+		0b00000000,
+	],
+	8 => [ # Poop
+		0b01000100,
+		0b00010000,
+		0b00111000,
+		0b01000100,
+		0b01111100,
+		0b10000010,
+		0b11111110,
+		0b00000000,
+	],
+	9 => [ # Heart
+		0b01101100,
+		0b10010010,
+		0b10010010,
+		0b10000010,
+		0b01000100,
+		0b00101000,
+		0b00010000,
+		0b00000000,
+	],
+	10 => [ # Spade
+		0b00010000,
+		0b00111000,
+		0b01111100,
+		0b11111110,
+		0b11111110,
+		0b00010000,
+		0b11111110,
+		0b00000000,
+	],
+	11 => [ # Club, TODO make better
+		0b00111000,
+		0b00111000,
+		0b11111110,
+		0b11111110,
+		0b11010110,
+		0b00010000,
+		0b00111000,
+		0b00000000,
+	],
+	12 => [ # Diamond
+		0b00010000,
+		0b00101000,
+		0b01000100,
+		0b10000010,
+		0b01000100,
+		0b00101000,
+		0b00010000,
+		0b00000000,
+	],
+	13 => [ # Table
+		0b00000000,
+		0b00000000,
+		0b00000000,
+		0b11111110,
+		0b01000100,
+		0b01000100,
+		0b01000100,
+		0b00000000,
+	],
+	14 => [ # Warning
+		0b01100000,
+		0b11110000,
+		0b11110000,
+		0b01100000,
+		0b01100000,
+		0b00000000,
+		0b01100000,
+		0b00000000, # force keep this blank column
+	],
+	15 => [ # Error
+		0b00010000,
+		0b00111000,
+		0b00111000,
+		0b01111100,
+		0b01111100,
+		0b11111110,
+		0b11111110,
+		0b00000000, # force keep this blank column
+	],
+	16 => [ #
+		0b00000000,
+		0b00000000,
+		0b00000000,
+		0b00000000,
+		0b00000000,
+		0b00000000,
+		0b00000000,
+		0b00000000, # force keep this blank column
+	],
+	17 => [ # Triforce, this one made me need larger widths
+		0b0000010000000000,
+		0b0000111000000000,
+		0b0001111100000000,
+		0b0010000010000000,
+		0b0111000111000000,
+		0b1111101111100000,
+		0b0000000000000000,
   ],
-  1 => [ # Smiley 
-    0b00000000,
-    0b01101100,
-    0b01101100,
-    0b00000000,
-    0b00000000,
-    0b10000010,
-    0b01111100,
-    0b00000000,
-  ],
-  2 => [ # Frowny
-    0b00000000,
-    0b01101100,
-    0b01101100,
-    0b00000000,
-    0b00000000,
-    0b01111100,
-    0b10000010,
-    0b00000000,
-  ],
-  3 => [ # Winky
-    0b00000000,
-    0b01001100,
-    0b00001100,
-    0b00000000,
-    0b00000000,
-    0b10000010,
-    0b01111100,
-    0b00000000,
-  ],
-  4 => [ # Excited
-    0b00000000,
-    0b01101100,
-    0b01101100,
-    0b00000000,
-    0b11111110,
-    0b10000010,
-    0b01111100,
-    0b00000000,
-  ],
-  5 => [ # ? not sure yet TODO
-    0b00000000,
-    0b01001100,
-    0b00001100,
-    0b00000000,
-    0b00000000,
-    0b10000010,
-    0b01111100,
-    0b00000000,
-  ],
-  6 => [ # Mug
-    0b10001000,
-    0b10001110,
-    0b10001001,
-    0b10001001,
-    0b10001001,
-    0b10001110,
-    0b11111000,
-    0b00000000,
-  ],
-  7 => [ # Music note
-    0b00011100,
-    0b00010010,
-    0b00010000,
-    0b00010000,
-    0b00010000,
-    0b01100000,
-    0b01100000,
-    0b00000000,
-  ],
-  8 => [ # Poop
-    0b01000100,
-    0b00010000,
-    0b00111000,
-    0b01000100,
-    0b01111100,
-    0b10000010,
-    0b11111110,
-    0b00000000,
-  ],
-  9 => [ # Heart
-    0b01101100,
-    0b10010010,
-    0b10010010,
-    0b10000010,
-    0b01000100,
-    0b00101000,
-    0b00010000,
-    0b00000000,
-  ],
-  10 => [ # Spade
-    0b00010000,
-    0b00111000,
-    0b01111100,
-    0b11111110,
-    0b11111110,
-    0b00010000,
-    0b11111110,
-    0b00000000,
-  ],
-  11 => [ # Club, TODO make better
-    0b00111000,
-    0b00111000,
-    0b11111110,
-    0b11111110,
-    0b11010110,
-    0b00010000,
-    0b00111000,
-    0b00000000,
-  ],
-  12 => [ # Diamond
-    0b00010000,
-    0b00101000,
-    0b01000100,
-    0b10000010,
-    0b01000100,
-    0b00101000,
-    0b00010000,
-    0b00000000,
-  ],
-  13 => [ # Table  
-    0b00000000,
-    0b00000000,
-    0b00000000,
-    0b11111110,
-    0b01000100,
-    0b01000100,
-    0b01000100,
-    0b00000000,
-  ],
-  14 => [ # Warning
-    0b01100000,
-    0b11110000,
-    0b11110000,
-    0b01100000,
-    0b01100000,
-    0b00000000,
-    0b01100000,
-    0b00000000, # force keep this blank column
-  ],
-  15 => [ # Error
-    0b00010000,
-    0b00111000,
-    0b00111000,
-    0b01111100,
-    0b01111100,
-    0b11111110,
-    0b11111110,
-    0b00000000, # force keep this blank column
-  ],
-  16 => [ # 
-    0b00000000,
-    0b00000000,
-    0b00000000,
-    0b00000000,
-    0b00000000,
-    0b00000000,
-    0b00000000,
-    0b00000000, # force keep this blank column
-  ],
-
-  # The following are because the characters are too high, or in the wrong spots
-  ord("j") => [
-    0b00100000,
-    0b00000000,
-    0b00100000,
-    0b00100000,
-    0b00100000,
-    0b00100000,
-    0b01000000,
-    0b00000000, # force keep this blank column
-  ],
-  ord("q") => [
-    0b00000000,
-    0b00000000,
-    0b01100000,
-    0b10010000,
-    0b10010000,
+  18 => [ # Mario coin
     0b01110000,
-    0b00010000,
-    0b00000000, # force keep this blank column
-  ],
-  ord("p") => [
-    0b00000000,
-    0b00000000,
-    0b01100000,
-    0b10010000,
-    0b10010000,
-    0b11100000,
-    0b10000000,
-    0b00000000, # force keep this blank column
-  ],
-  ord("g") => [
-    0b00000000,
-    0b01100000,
-    0b10010000,
-    0b10010000,
-    0b01110000,
-    0b00010000,
-    0b11100000,
-    0b00000000, # force keep this blank column
-  ],
-  ord("y") => [
-    0b00000000,
-    0b00000000,
-    0b10010000,
-    0b10010000,
-    0b01110000,
-    0b00010000,
-    0b11100000,
-    0b00000000, # force keep this blank column
-  ],
-  ord("Q") => [
-    0b01100000,
-    0b10010000,
-    0b10010000,
-    0b10010000,
-    0b10010000,
-    0b10110000,
-    0b01101000,
-    0b00000000, # force keep this blank column
-  ],
-  ord("V") => [
     0b10001000,
-    0b10001000,
-    0b10001000,
-    0b01010000,
-    0b01010000,
-    0b00100000,
-    0b00100000,
-    0b00000000, # force keep this blank column
-  ],
-  ord("W") => [
-    0b10000010,
-    0b10000010,
-    0b10010010,
-    0b10010010,
-    0b10010010,
-    0b10010010,
-    0b01101100,
-    0b00000000, # force keep this blank column
-  ],
-  ord(",") => [
-    0b00000000,
-    0b00000000,
-    0b00000000,
-    0b00000000,
-    0b00100000,
-    0b00100000,
-    0b01000000,
-    0b00000000, # force keep this blank column
-  ],
-  ord(".") => [
-    0b00000000,
-    0b00000000,
-    0b00000000,
-    0b00000000,
-    0b00000000,
-    0b01100000,
-    0b01100000,
-    0b00000000, # force keep this blank column
-  ],
-  ord(":") => [
-    0b00000000,
-    0b00000000,
-    0b01000000,
-    0b01000000,
-    0b00000000,
-    0b01000000,
-    0b01000000,
-    0b00000000, # force keep this blank column
-  ],
-  ord(";") => [
-    0b00000000,
-    0b00000000,
-    0b01000000,
-    0b01000000,
-    0b00000000,
-    0b01000000,
-    0b10000000,
-    0b00000000, # force keep this blank column
-  ],
-  ord("_") => [
-    0b00000000,
-    0b00000000,
-    0b00000000,
-    0b00000000,
-    0b00000000,
-    0b00000000,
-    0b11111000,
-    0b00000000, # force keep this blank column
-  ],
-
-
-
-
-  # Formatting meta-chars
-  31 => [ # Clear formatting
-    0b01010000,
-    0b10010000,
-    0b01011000,
-    0b00000000,
-    0b11000000,
-    0b10000000,
-    0b10000000,
-    0b00100000, # force keep this blank column
-  ],
-  30 => [ # Blink
-    0b10010000,
-    0b11010000,
-    0b11011000,
-    0b00000000,
-    0b10100000,
-    0b11000000,
-    0b10100000,
-    0b00000000, # force keep this blank column
-  ],
-  29 => [ # Inverse
-    0b11100000,
-    0b01001100,
-    0b11101010,
-    0b00000000,
-    0b10100000,
-    0b01000000,
-    0b00000000,
-    0b00010000, # force keep this blank column
-  ],
-  28 => [ # Inverse-Blink, invert instead of disappearing when blinking
-    0b11100000,
-    0b01001100,
-    0b11101010,
-    0b00000000,
     0b10101000,
-    0b01001100,
-    0b00001100,
-    0b00010000, # force keep this blank column
+    0b10101000,
+    0b10101000,
+    0b10001000,
+    0b01110000,
   ],
+  19 => [ # Mario mushroom
+    0b001111100,
+    0b010011110,
+    0b110110011,
+    0b111111111,
+    0b001000100,
+    0b001000100,
+    0b000111000,
+  ],
+  20 => [ # Metroid
+    0b001111000,
+    0b010100100,
+    0b101000010,
+    0b111111110,
+    0b010100100,
+    0b100100010,
+    0b000000000,
+  ],
+  
+
+
+	  # The following are because the characters are too high, or in the wrong spots
+	  ord("j") => [
+		0b00100000,
+		0b00000000,
+		0b00100000,
+		0b00100000,
+		0b00100000,
+		0b00100000,
+		0b01000000,
+		0b00000000, # force keep this blank column
+	  ],
+	ord("q") => [
+		0b00000000,
+		0b00000000,
+		0b01100000,
+		0b10010000,
+		0b10010000,
+		0b01110000,
+		0b00010000,
+		0b00000000, # force keep this blank column
+	],
+	ord("p") => [
+		0b00000000,
+		0b00000000,
+		0b01100000,
+		0b10010000,
+		0b10010000,
+		0b11100000,
+		0b10000000,
+		0b00000000, # force keep this blank column
+	],
+	ord("g") => [
+		0b00000000,
+		0b01100000,
+		0b10010000,
+		0b10010000,
+		0b01110000,
+		0b00010000,
+		0b11100000,
+		0b00000000, # force keep this blank column
+	],
+	ord("y") => [
+		0b00000000,
+		0b00000000,
+		0b10010000,
+		0b10010000,
+		0b01110000,
+		0b00010000,
+		0b11100000,
+		0b00000000, # force keep this blank column
+	],
+	ord("Q") => [
+		0b01100000,
+		0b10010000,
+		0b10010000,
+		0b10010000,
+		0b10010000,
+		0b10110000,
+		0b01101000,
+		0b00000000, # force keep this blank column
+	],
+	ord("V") => [
+		0b10001000,
+		0b10001000,
+		0b10001000,
+		0b01010000,
+		0b01010000,
+		0b00100000,
+		0b00100000,
+		0b00000000, # force keep this blank column
+	],
+	ord("W") => [
+		0b10000010,
+		0b10000010,
+		0b10010010,
+		0b10010010,
+		0b10010010,
+		0b10010010,
+		0b01101100,
+		0b00000000, # force keep this blank column
+	],
+	ord(",") => [
+		0b00000000,
+		0b00000000,
+		0b00000000,
+		0b00000000,
+		0b00100000,
+		0b00100000,
+		0b01000000,
+		0b00000000, # force keep this blank column
+	],
+	ord(".") => [
+		0b00000000,
+		0b00000000,
+		0b00000000,
+		0b00000000,
+		0b00000000,
+		0b01100000,
+		0b01100000,
+		0b00000000, # force keep this blank column
+	],
+	ord(":") => [
+		0b00000000,
+		0b00000000,
+		0b01000000,
+		0b01000000,
+		0b00000000,
+		0b01000000,
+		0b01000000,
+		0b00000000, # force keep this blank column
+	],
+	ord(";") => [
+		0b00000000,
+		0b00000000,
+		0b01000000,
+		0b01000000,
+		0b00000000,
+		0b01000000,
+		0b10000000,
+		0b00000000, # force keep this blank column
+	],
+	ord("_") => [
+		0b00000000,
+		0b00000000,
+		0b00000000,
+		0b00000000,
+		0b00000000,
+		0b00000000,
+		0b11111000,
+		0b00000000, # force keep this blank column
+	],
+
+
+	# Formatting meta-chars
+	31 => [ # Clear formatting
+		0b01010000,
+		0b10010000,
+		0b01011000,
+		0b00000000,
+		0b11000000,
+		0b10000000,
+		0b10000000,
+		0b00100000, # force keep this blank column
+	],
+	30 => [ # Blink
+		0b10010000,
+		0b11010000,
+		0b11011000,
+		0b00000000,
+		0b10100000,
+		0b11000000,
+		0b10100000,
+		0b00000000, # force keep this blank column
+	],
+	29 => [ # Inverse
+		0b11100000,
+		0b01001100,
+		0b11101010,
+		0b00000000,
+		0b10100000,
+		0b01000000,
+		0b00000000,
+		0b00010000, # force keep this blank column
+	],
+	28 => [ # Inverse-Blink, invert instead of disappearing when blinking
+		0b11100000,
+		0b01001100,
+		0b11101010,
+		0b00000000,
+		0b10101000,
+		0b01001100,
+		0b00001100,
+		0b00010000, # force keep this blank column
+	],
 };
 
 {
-  package MyGlyphs;
-  use Data::Dumper;
 
-  sub bitmap {
-    print Dumper($_[0], $custom_chars->{${$_[0]}});
-    return [map {$_ << 24} $custom_chars->{$_[0]->$*}->@*];
-  }
+	package MyGlyphs;
+	use Data::Dumper;
 
-  sub width {
-    return 8
-  }
 
-  sub name {
-    return "CUSTOM";
-  }
+	sub bitmap {
+		print Dumper($_[0], $custom_chars->{${$_[0]}});
+		return [map {$_ << 16} $custom_chars->{$_[0]->$*}->@*];
+	}
+
+
+	sub width {
+		return 16;
+	}
+
+
+	sub name {
+		return "CUSTOM";
+	}
 }
+
 
 sub printbits {
-   my ( $bits ) = @_;
-   while( $bits ) {
-      print +( $bits & (1<<31) ) ? '#' : ' ';
-      $bits <<= 1;
-   }
-   print "\n";
+	my ($bits) = @_;
+	while($bits) {
+		print +( $bits & (1<<31) ) ? '#' : ' ';
+		$bits <<= 1;
+	}
+	print "\n";
 }
 
+
 sub transpose_glyph {
-  my $g = shift;
+	my $g = shift;
 
-  my @raw_bits = map {[split('', substr(sprintf("%032b", $_), 0, $g->width))]} $g->bitmap->@*;
-  my @new_bits;
+	my @raw_bits = map {[split('', substr(sprintf("%032b", $_), 0, $g->width))]} $g->bitmap->@*;
+	my @new_bits;
 
-  for my $row (0..$#raw_bits) {
-    for my $col (0..$g->width-1) {
-      $new_bits[$col][$row] = $raw_bits[$row][$col];
-    }
-  }
+	for my $row (0..$#raw_bits) {
+		for my $col (0..$g->width-1) {
+			$new_bits[$col][$row] = $raw_bits[$row][$col];
+		}
+	}
 
-  # they're not cis bits
-  my @trans_bits = grep {$_ ne "0b00000000"} map {"0b".join '', $_->@*} @new_bits;
+	# they're not cis bits
+	my @trans_bits = grep {$_ !~ /0b0+$/} map {"0b".join '', $_->@*} @new_bits;
 
-  push @trans_bits, "0b00000000"; # Always ensure we have at least one column, and that it's blank at the end.  This adds kerning and prevents issues with some characters that are blank
-  push @trans_bits, "0b00000000" if $g->name eq ' '; # make sure space is at least two pixels wide.
-  unshift @trans_bits, "0b00000000" if $g->name eq '.'; # center the . a bit to balance things
+	push @trans_bits, "0b00000000"; # Always ensure we have at least one column, and that it's blank at the end.  This adds kerning and prevents issues with some characters that are blank
+	push @trans_bits, "0b00000000" if $g->name eq ' '; # make sure space is at least two pixels wide.
+	unshift @trans_bits, "0b00000000" if $g->name eq '.'; # center the . a bit to balance things
 
-#  print Dumper(\@trans_bits);
-  return {width => scalar @trans_bits, columns => \@trans_bits}; 
+	#  print Dumper(\@trans_bits);
+	return {width => scalar @trans_bits, columns => \@trans_bits};
 }
 
 my @bytes;
 my @widths;
 
-my $font = Font::PCF->open( "/usr/share/fonts/X11/misc/clR5x8.pcf.gz" );
+my $font = Font::PCF->open("/usr/share/fonts/X11/misc/clR5x8.pcf.gz");
 
 for my $char (0..127) {
-  my $g;
+	my $g;
 
-  if (exists($custom_chars->{$char})) {
-    $g = bless \$char, "MyGlyphs";
-  } else {
-   $g = $font->get_glyph_for_char( chr $char );
-  }
-  my $glyph = transpose_glyph $g;
+	if (exists($custom_chars->{$char})) {
+		$g = bless \$char, "MyGlyphs";
+	} else {
+		$g = $font->get_glyph_for_char( chr $char );
+	}
+	my $glyph = transpose_glyph $g;
 
-  push @bytes, $glyph->{columns}->@*;
-  push @widths, $glyph->{width};
+	push @bytes, $glyph->{columns}->@*;
+	push @widths, $glyph->{width};
 }
 
 my $template = <<"EOT";
 
-uint8_t font_glyphs[] = {
+const PROGMEM uint8_t font_glyphs[] = {
     %B%
 };
-uint8_t font_starts[] = {%S%};
-uint8_t font_widths[] = {%W%};
+const PROGMEM uint8_t font_starts[] = {%S%};
+const PROGMEM uint8_t font_widths[] = {%W%};
 
 EOT
 
@@ -449,14 +491,14 @@ my $white = $gd->colorAllocate(255,255,255);
 my $black = $gd->colorAllocate(0,0,0);
 
 for my $x (0..$#bytes) {
-  my $col = $bytes[$x];
-  for my $y (0..7) {
-    my $bit = oct($col) & (2**$y) ? 1 : 0;
-    
-    if ($bit) {
-      $gd->setPixel($x, 7-$y, $black)
-    }
-  }
+	my $col = $bytes[$x];
+	for my $y (0..7) {
+		my $bit = oct($col) & (2**$y) ? 1 : 0;
+
+		if ($bit) {
+			$gd->setPixel($x, 7-$y, $black);
+		}
+	}
 }
 
 open(my $imfh, ">image.png");
